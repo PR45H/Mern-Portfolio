270ccfe5d7f4cc1ef2a763c67d0012d42e5f9e17
PR45H <Prashantpillai7@gmail.com>
2024-08-08 01:19:04 +0530

    created an about page and component for section title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import About from './About'
 
 const Home = () => {
     return (
-        <div className='px-20 bg-primary'>
+        <div className='px-20 bg-primary selection:bg-selection'>
             <Header />
             <Intro />
             <About />
