bf7f5c358e7df2a941833b713a5584d3988cb0ff
PR45H <Prashantpillai7@gmail.com>
2024-08-14 00:30:56 +0530

    data file for project created

---
+++ src/resources/project.js
@@ -0,0 +1,31 @@
+import tata1mg from '../assets/1mg-website-image.webp'
+import hospitalStaff from '../assets/hospital-staff-record.webp'
+
+export const projectData = [
+    {
+        id: 1,
+        title: "Tata 1mg Clone",
+        description: "A clone of the Tata 1mg website, built using HTML CSS & Vanilla Js.",
+        techStack: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/PR45H/1MG-Clone/tree/main",
+        deployed: "https://magnificent-maamoul-a836e1.netlify.app",
+        image: tata1mg,
+    },
+    {
+        id: 2,
+        title: "Hospital staff record",
+        description: "A simple CRUD application to manage hospital staff records.",
+        techStack: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/PR45H/Hospital-Staff-Record-Handeler",
+        deployed: "https://poetic-medovik-ce5f5f.netlify.app",
+        image: hospitalStaff,
+    },
+    {
+        id: 3,
+        title: "AliExpress Clone",
+        description: "A clone of the AliExpress website, built using MERN stack. It is a work in progress project.",
+        techStack: ["MongoDB", "Express", "React", "Node.js"],
+        github: "https://github.com/PR45H/AliExpress-Clone",
+        image: "https://via.placeholder.com/150",
+    },
+]
