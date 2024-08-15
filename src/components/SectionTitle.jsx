2548450124195ce14b88a661cb43014a19978175
PR45H <Prashantpillai7@gmail.com>
2024-08-15 18:54:10 +0530

    changed title color

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const SectionTitle = ({title}) => {
     return (
         <div className='flex items-center gap-10 mt-20'>
-            <h1 className='text-white font-medium text-3xl'>{title}</h1>
+            <h1 className='text-secondary font-medium text-3xl'>{title}</h1>
             <div className='w-1/4 h-0.5 opacity-40 bg-tertiary'></div>
 
         </div>
