8c1c7dbb1783e16177b4ce061dc9176b894e183a
PR45H <Prashantpillai7@gmail.com>
2024-08-15 20:37:50 +0530

    leftSide contact icon and a tag implemented

--- src/pages/Home/LeftSider.jsx
+++ src/pages/Home/LeftSider.jsx
@@ -3,12 +3,30 @@ import React from 'react'
 const LeftSider = () => {
     return (
         <div>
-            <div className="flex flex-col gap-5">
-                <i class="ri-facebook-line text-xl cursor-pointer text-gray-700  w-fit"></i>
-                <i class="ri-instagram-line text-xl cursor-pointer text-gray-700  w-fit"></i>
-                <i class="ri-linkedin-line text-xl cursor-pointer text-gray-700  w-fit"></i>
-                <i class="ri-github-line text-xl cursor-pointer text-gray-700  w-fit"></i>
-                <i class="ri-mail-line text-xl cursor-pointer text-gray-700  w-fit"></i>
+            <div className="flex flex-col gap-3 items-center bottom-0 left-5 fixed lg:static">
+                <div className="flex flex-col gap-5 lg:flex-row">
+                    <a href="https://www.facebook.com/prashant.babu.520/" target='_blank'>
+                        <i className="ri-facebook-line text-xl cursor-pointer text-gray-700  w-fit"></i>
+                    </a>
+
+                    <a href="https://www.instagram.com/beatsinbits/" target='_blank'>
+                        <i className="ri-instagram-line text-xl cursor-pointer text-gray-700  w-fit"></i>
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/prashant-babu-b157aa247/" target='_blank'>
+                        <i className="ri-linkedin-line text-xl cursor-pointer text-gray-700  w-fit"></i>
+                    </a>
+
+                    <a href="mailto:prashantpillai7@gmail.com" target='_blank'>
+                        <i className="ri-mail-line text-xl cursor-pointer text-gray-700  w-fit"></i>
+                    </a>
+
+                    <a href="https://github.com/PR45H" target='_blank'>
+                        <i className="ri-github-line text-xl cursor-pointer text-gray-700  w-fit">  </i>        
+                    </a>
+                </div>
+
+                <div className="h-52 w-[1px] bg-gray-800 lg:hidden"></div>
 
             </div>
         </div>
