fc20446477a8b0d8d340122f7b84c7fa5d681391
PR45H <Prashantpillai7@gmail.com>
2024-08-16 01:03:56 +0530

    feat: Connect backend to MongoDB database

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connection = require('./config/db.connection');
 
 // health check
 app.get('/', (req, res) => {
     res.send('Server responsed')
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(port, async () => {
+    try {
+        await connection;
+        console.log(`Server is running on port ${process.env.PORT} & database is connected`);
+        
+    } catch (error) {
+        console.log(`Server is not running due to ${error}`);
+        
+    }
 })
