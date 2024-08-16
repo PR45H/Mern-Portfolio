8f62bbed07d8270f1c2dee80b42aee105780a858
PR45H <Prashantpillai7@gmail.com>
2024-08-16 23:23:27 +0530

    feat: Add portfolio model and schemas to backend
    The code changes add a new file `portfolio.model.js` to the backend models directory. This commit introduces the portfolio model and associated schemas for intro, about, skill, project, and contact. These schemas will be used to store and retrieve portfolio data from the MongoDB database.

--- backend/models/portfolio.model.js
+++ backend/models/portfolio.model.js
@@ -1,1 +1,101 @@
 const mongoose = require('mongoose');
+
+const introSchema = new mongoose.Schema({
+    welcomeText: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    caption: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+});
+
+const aboutSchema = new mongoose.Schema({
+    lottieUrl: {
+        type: String,
+        required: true
+    },
+    description1: {
+        type: String,
+    },
+    description2: {
+        type: String,
+    },
+    description3: {
+        type: String,
+    },
+    description4: {
+        type: String,
+    },
+});
+
+const skillSchema = new mongoose.Schema({
+    skillName: {
+        type: Array,
+        required: true
+    },
+    
+});
+
+const projectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+});
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = {
+    Intro: mongoose.model('Intro', introSchema),
+    About: mongoose.model('About', aboutSchema),
+    Skill: mongoose.model('Skill', skillSchema),
+    Project: mongoose.model('Project', projectSchema),
+    Contact: mongoose.model('Contact', contactSchema),
+}
