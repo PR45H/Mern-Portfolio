eadb5147e9f0f98443854b0f0aed8d6012fdf52f
PR45H <Prashantpillai7@gmail.com>
2024-08-21 23:16:48 +0530

    refactor: Add CORS middleware to backend
    The code changes add the CORS middleware to the backend by importing and using the 'cors' package. This commit enables Cross-Origin Resource Sharing (CORS) for the backend server, allowing it to accept requests from different origins. This is important for handling API requests from the frontend or other external sources.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const connection = require('./config/db.connection');
 const portfolioRouter = require('./routes/portfolioRouter');
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/portfolio', portfolioRouter);
 
