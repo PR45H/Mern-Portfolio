31edbbe68a1e9dbc5bbb8225a0e62a74f236809b
PR45H <Prashantpillai7@gmail.com>
2024-08-27 18:11:31 +0530

    refactor: Update rootSlice for Redux state management
    The code changes in 'rootSlice.js' update the rootSlice for Redux state management. The initialState is modified to include isLoading and portfollioData properties. The reducers are also updated to include ShowLoading, HideLoading, and SetPortfollioData actions. This commit improves the state management in the application.

--- frontend/src/redux/rootSlice.js
+++ frontend/src/redux/rootSlice.js
@@ -2,5 +2,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const rootSlice = createSlice({
-    
+    name: 'root',
+    initialState: {
+        isLoading: false,
+        portfollioData: null
+    },
+    reducers: {
+        ShowLoading: (state) => {
+            state.isLoading = true;
+        },
+        HideLoading: (state) => {
+            state.isLoading = false;
+        },
+        SetPortfollioData: (state, action) => {
+            state.portfollioData = action.payload;
+        }
+    }
 })
+
+export default rootSlice.reducer;
+export const { ShowLoading, HideLoading, SetPortfollioData } = rootSlice.actions;
