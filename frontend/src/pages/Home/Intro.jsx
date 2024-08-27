54e7fda3ac37f2d24426dfd9662f834debd950a2
PR45H <Prashantpillai7@gmail.com>
2024-08-27 23:38:35 +0530

    refactor: Update Intro component to use Redux store for portfolio data
    The code changes in 'Intro.jsx' modify the Intro component to use the Redux store for retrieving the portfolio data. The useSelector hook is used to access the portfolioData from the root state. This refactor improves the data flow and ensures consistency in accessing the portfolio data throughout the application.

--- frontend/src/pages/Home/Intro.jsx
+++ frontend/src/pages/Home/Intro.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import About from './About'
+import { useSelector } from 'react-redux'
 
 const Intro = () => {
+    // get the portfolioData from the redux store
+    const { portfolioData } = useSelector((state) => state.root)
+    const {intro}  = portfolioData
+    const {welcomeText , firstName, lastName, caption, description} = intro[0]
+    
     return (
             <div className="h-fit bg-primary flex flex-col items-start text-wrap justify-center gap-8 pt-8">
             <h1 className="text-xl font-semibold text-center text-white">
-                Hi, I am
+                {welcomeText}
             </h1>
             <p className="text-secondary font-bold text-6xl sm:text-3xl lg:text-4xl text-center">
-                Prashant Babu
+                {firstName} {lastName}
             </p>
             <p className="text-white text-6xl font-bold sm:text-xl lg:text-4xl text-center">
-                I build things for the web
+                {caption}
             </p>
             
             <div className="h-fit text-wrap">
                 <p className="text-white text-s font-medium text-opacity-60 w-3/4 lg:w-full">
-                    I’m a passionate Full Stack Web Developer specializing in the
-                    MERN stack. As a fresher, I’m enthusiastic about joining a
-                    dynamic team and contributing to impactful solutions. Let’s connect and
-                    create something amazing together!
+                    {description}
                 </p>
             </div>
 
