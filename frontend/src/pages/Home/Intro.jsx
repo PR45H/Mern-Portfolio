16ac60a4a57c7cb26c8b7404cb80f9acb836117e
PR45H <Prashantpillai7@gmail.com>
2024-08-28 18:52:46 +0530

    refactor: Update Intro component to handle empty welcomeText, firstName, lastName, and caption
    The code changes in 'Intro.jsx' modify the Intro component to handle empty values for welcomeText, firstName, lastName, and caption. This ensures that the component can gracefully handle scenarios where these values are not provided. This refactor improves the robustness and user experience of the application.

--- frontend/src/pages/Home/Intro.jsx
+++ frontend/src/pages/Home/Intro.jsx
@@ -10,24 +10,24 @@ const Intro = () => {
     return (
             <div className="h-fit bg-primary flex flex-col items-start text-wrap justify-center gap-8 pt-8">
             <h1 className="text-xl font-semibold text-center text-white">
-                {welcomeText}
+                {welcomeText || ""}
             </h1>
             <p className="text-secondary font-bold text-6xl sm:text-3xl lg:text-4xl text-center">
-                {firstName} {lastName}
+                {firstName || ""} {lastName || ""}
             </p>
             <p className="text-white text-6xl font-bold sm:text-xl lg:text-4xl text-center">
-                {caption}
+                {caption || ""}
             </p>
             
             <div className="h-fit text-wrap">
                 <p className="text-white text-s font-medium text-opacity-60 w-3/4 lg:w-full">
-                    {description}
+                    {description || ""}
                 </p>
             </div>
-
+{/* 
             <div>
                 <button className="border-2 border-tertiary px-10 py-4 text-lg text-tertiary rounded-md">Get Started</button>
-            </div>
+            </div> */}
         </div>
     )
 }
