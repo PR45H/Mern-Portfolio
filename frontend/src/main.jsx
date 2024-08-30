19fe42e9359f7231bbf31eb00c7e7a76482e6f81
PR45H <Prashantpillai7@gmail.com>
2024-08-31 01:24:44 +0530

    refactor: Update main.jsx to use ChakraProvider and RouterProvider
    The code changes in 'main.jsx' update the rendering of the application by wrapping the root component with the ChakraProvider from the @chakra-ui/react library and the RouterProvider from the react-router-dom library. This change ensures consistent UI styling throughout the application and enables routing functionality. This refactor improves the overall visual appeal, user experience, and navigation of the application.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {Provider} from 'react-redux'
-import store from './redux/store.js'
-import { ChakraProvider } from "@chakra-ui/react"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+import { ChakraProvider } from '@chakra-ui/react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './App.jsx'; // Import the router
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </Provider>
-)
+  <ChakraProvider>
+    <Provider store={store}>
+      {/* Use RouterProvider to provide the router */}
+      <RouterProvider router={router} />
+    </Provider>
+  </ChakraProvider>
+);
