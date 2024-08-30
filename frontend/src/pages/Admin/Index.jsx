58f28d2a423913eb2c8eef280ae7f45de37baef2
PR45H <Prashantpillai7@gmail.com>
2024-08-30 21:42:44 +0530

    refactor: Update main.jsx to use ChakraProvider for consistent UI styling

--- frontend/src/pages/Admin/Index.jsx
+++ frontend/src/pages/Admin/Index.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
-import { Tabs, Tooltip } from "antd";
-import AdminIntro from './AdminIntro'
-import AdminAbout from './AdminAbout'
-import Header from '../../components/Header';
 
-const Index = () => {
-    const items = [
-            {
-            key: '1',
-            label: 'Admin Intro',
-            children: <AdminIntro />,
-            },
-            {
-            key: '2',
-            label: 'Admin About',
-            children: <AdminAbout />,
-            }]
+
+const index = () => {
     return (
-        <div >
-            <Header />
-            <Tabs defaultActiveKey="1" items={items} className='mt-5' centered />
+        <div>
             
         </div>
-        
     )
 }
 
-export default Index
+export default index
