2f083e8f2af85dfee3100b34b18a01089f3aaf87
PR45H <Prashantpillai7@gmail.com>
2024-08-31 01:25:05 +0530

    refactor: Update App component to use createBrowserRouter for routing
    The code changes in 'App.jsx' update the routing implementation by replacing the BrowserRouter component with the createBrowserRouter function from the react-router-dom library. This change allows for more flexibility in defining and managing routes within the application. This refactor improves the overall navigation and user experience of the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,56 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter ,Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import axios from 'axios'
-import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { HideLoading, SetPortfolioData, ShowLoading } from './redux/rootSlice'
-import Loader from './components/Loader'
-import Index from './pages/Admin/Index'
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom'; // Import Outlet to render child routes
+import axios from 'axios';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { HideLoading, SetPortfolioData, ShowLoading } from './redux/rootSlice';
+import Loader from './components/Loader';
+import Home from './pages/Home/Home';
+import Index from './pages/Admin/Index';
+import { createActions } from './pages/Admin/AdminIntro';
+import { createBrowserRouter } from 'react-router-dom';
+
+// Define the routes using createBrowserRouter
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // Use App as the parent layout component
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/admin', element: <Index />, action: createActions }, // Add action here
+    ],
+  },
+]);
 
 function App() {
+  const { portfolioData, isLoading } = useSelector((state) => state.root);
+  const dispatch = useDispatch();
 
-  const {portfolioData , isLoading} = useSelector((state)=> state.root)
-  // connect to the backend
-  const dispatch = useDispatch()
   const getPortfolioData = async () => {
     try {
-      dispatch(ShowLoading())
+      dispatch(ShowLoading());
       const response = await axios.get('/api/portfolio/get-portfolio-data');
-      dispatch(SetPortfolioData(response.data))
-      dispatch(HideLoading())
+      dispatch(SetPortfolioData(response.data));
+      dispatch(HideLoading());
     } catch (error) {
-      console.error(error)
-      dispatch(HideLoading())
+      console.error(error);
+      dispatch(HideLoading());
     }
-  }
+  };
 
   useEffect(() => {
     if (!portfolioData) {
-      getPortfolioData()
-      
+      getPortfolioData();
     }
-  },[portfolioData])
-
+  }, [portfolioData]);
 
   return (
     <div>
-        <BrowserRouter>
-          {isLoading && <Loader /> }
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/admin" element={<Index />} />
-          </Routes>
-        </BrowserRouter>
+      {isLoading && <Loader />}
+      {/* The Outlet component renders the matched child route's component */}
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
