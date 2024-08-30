5ae9d7f6a6fb42b1494b89d64bb73cb46ebdbeff
PR45H <Prashantpillai7@gmail.com>
2024-08-31 01:25:29 +0530

    refactor: Update AdminIntro.jsx to use Chakra UI form components
    The code changes in 'AdminIntro.jsx' update the rendering of the admin introduction page by replacing the custom form elements with the Chakra UI form components. This change improves the consistency and visual appeal of the form inputs, providing a more modern and user-friendly experience. This refactor enhances the overall usability and aesthetics of the admin introduction page.

--- frontend/src/pages/Admin/AdminIntro.jsx
+++ frontend/src/pages/Admin/AdminIntro.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react'
+import { Form, redirect } from 'react-router-dom'
 
 const AdminIntro = () => {
+
     return (
-      <div>
-        
+      <div className='flex flex-col gap-5 sm:gap-10'>
+        <Form method='post' action='/admin' >
+          <FormControl >
+            <FormLabel>Welcome Text</FormLabel>
+            <Input type="text" placeholder='Hey, I am' name='welcomeText'/>
+          </FormControl>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input type='text' placeholder='Jon' name='firstName' />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input type='text' placeholder='Doe' name='lastName' />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Caption</FormLabel>
+            <Input type='text' placeholder='I build things for web...' name='caption' />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Description</FormLabel>
+            <Textarea placeholder='A short description of your journey and skills' name='description'/>
+          </FormControl>
+          <Button colorScheme='teal' type='submit' className='sm:mt-10 sm:w-full w-24 mt-10  '>Save</Button>
+        </Form>
       </div>
     )
 }
+export const createActions = async ({ request }) => {
+  try {
+    const data = await request.formData()
+    const submission = {
+      welcomeText: data.get('welcomeText'),
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
+      caption: data.get('caption'),
+      description: data.get('description')
+
+    }
+    console.log(submission);
+    
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+  return redirect('/admin')
+};
 
 export default AdminIntro
