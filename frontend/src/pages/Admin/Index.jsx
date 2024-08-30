1223a3fea99111386accb4edf17a69f06cbf6997
PR45H <Prashantpillai7@gmail.com>
2024-08-31 01:25:18 +0530

    refactor: Update Admin/Index.jsx to use Chakra UI Tabs for improved navigation
    The code changes in 'Admin/Index.jsx' update the rendering of the admin page by replacing the custom tab implementation with the Chakra UI Tabs component. This change allows for improved navigation within the admin section of the application, providing a more intuitive and user-friendly experience. This refactor enhances the overall usability and visual appeal of the admin page.

--- frontend/src/pages/Admin/Index.jsx
+++ frontend/src/pages/Admin/Index.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import AdminIntro from './AdminIntro'
+import Header from '../../components/Header'
+import { useSelector } from 'react-redux'
 
 const index = () => {
+    const {portfolioData} = useSelector((state)=> state.root)
     return (
         <div>
+            <Header />
+            {portfolioData && <Tabs className='mt-5 p-5 sm:' colorScheme='teal' >
+                <TabList >
+                    <Tab>Admin Intro</Tab>
+                    <Tab>Admin About</Tab>
+                    <Tab>Three</Tab>
+                </TabList>
+
+                <TabPanels>
+                    <TabPanel>
+                        <AdminIntro />
+                    </TabPanel>
+                    <TabPanel>
+                        <p>two!</p>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>three!</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>}
             
         </div>
     )
