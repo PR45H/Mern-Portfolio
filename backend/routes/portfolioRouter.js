c4db2b36683b49289aa3ddcf144aa9aa526c7cb5
PR45H <Prashantpillai7@gmail.com>
2024-09-06 01:38:56 +0530

    refactor: Update portfolioRouter.js to use controller functions for API routes

--- backend/routes/portfolioRouter.js
+++ backend/routes/portfolioRouter.js
@@ -1,51 +1,15 @@
 const portfolioRouter = require("express").Router();
-const {Intro, About, Skill, Project, Contact} = require('../models/portfolio.model');
-
+const { healthCheck, getPortfolioData, updateIntro, updateAbout } = require("../config/portfolio.controller")
 // api health check route
-portfolioRouter.get('/', (req, res) => {
-    res.send('Api Health check complete : Server responded')
-})
+portfolioRouter.get('/', healthCheck)
 
 // api route to get the portfolio data
-portfolioRouter.get('/get-portfolio-data', async (req, res) => {
-    try {
-        const intro = await Intro.find();
-        const about = await About.find();
-        const skill = await Skill.find();
-        const project = await Project.find();
-        const contact = await Contact.find();
-
-        res.status(200).send({
-            intro: intro,
-            about: about,
-            skills: skill,
-            projects: project,
-            contact: contact
-        });
-    } catch (error) {
-        res.status(500).send({message: error.message});
-    }
-});
+portfolioRouter.get('/get-portfolio-data', getPortfolioData);
 
 // api route to update the intro data
-portfolioRouter.post('/update-intro', async (req, res) => {
-    try {
-        const updatedData = await Intro.findOneAndUpdate(
-            {_id: req.body._id},
-            {
-                welcomeText: req.body.welcomeText,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                caption: req.body.caption,
-                description: req.body.description
-            }
-        )
-        res.status(200).send({data: updatedData, message: 'Intro updated successfully'})
+portfolioRouter.post('/update-intro', updateIntro);
 
-    } catch (error) {
-        res.status(500).send({message: error.message});
-        
-    }
-});
+// api route to update the about data
+portfolioRouter.post('/update-about', updateAbout);
 
 module.exports = portfolioRouter;
