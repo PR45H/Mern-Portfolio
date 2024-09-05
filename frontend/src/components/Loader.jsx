b9bc26515fe3931024c59f7a7776631e949d4a00
PR45H <Prashantpillai7@gmail.com>
2024-09-05 23:36:05 +0530

    refactor: Update Loader component to add z-index for proper layering
    The code changes in 'Loader.jsx' add the 'z-[100]' class to the root div element to set the z-index property. This ensures that the Loader component is properly layered above other elements on the page. This refactor improves the visual presentation and user experience of the Loader component.

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div className='h-screen flex justify-center items-center fixed inset-0 bg-primary'>
+        <div className='h-screen flex justify-center items-center fixed inset-0 bg-primary z-[100]'>
             <div className='text-6xl flex gap-10 font-semibold'>
                 <h1 className='text-secondary p'>P</h1>
                 <h1 className='text-white r'>R</h1>
